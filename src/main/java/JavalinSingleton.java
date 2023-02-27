86c2041f3f57c6444a1ad04447674183ad38a751
InZayneX <daoxiong2013@gmail.com>
2023-02-27 23:53:35 +0000

    done

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -17,11 +17,11 @@ public class JavalinSingleton {
         Javalin app = Javalin.create();
 
         app.get("/firstname/{first}", ctx -> {
-            
             //write code here
-
+            String firstName = ctx.pathParam("first");
+            //ctx.result("first" + ctx.pathParam("first"));
+            ctx.result(firstName);
         });
-
         return app;
     }
     
